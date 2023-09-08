3c86560856d1e08a89e0d20607440e10d84430f6
Mitchell <68230846+Creaous@users.noreply.github.com>
2023-09-08 12:01:19 +0930

    Update backup-all-luks-headers.sh
    - Put spaces between output messages
    - Added a chmod and chown feature

--- backup-all-luks-headers.sh
+++ backup-all-luks-headers.sh
@@ -3,11 +3,20 @@
 # Script downloaded from https://github.com/Creaous/Linux-Scripts
 # This script requires sudo permissions!
 
+# Check if the script is running with sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script with sudo or as root."
+    exit 1
+fi
+
 # Directory where you want to store the LUKS header backups
-backup_dir="." # Use . for current directory
+backup_dir="./backup/luks/headers" # Use . for current directory
 
 # Create the backup directory if it doesn't exist
 mkdir -p "$backup_dir"
+
+# Get the username of the currently logged-in user
+current_user=$(logname)
 
 # Get a list of LUKS-encrypted devices
 luks_devices=$(lsblk -o NAME,TYPE,FSTYPE | grep crypto_LUKS | sed 's/[├─└─]//g' | awk '{print $1}')
@@ -23,9 +32,23 @@ for device in $luks_devices; do
     
     if [ $? -eq 0 ]; then
         echo "Backup successful."
+        echo
     else
         echo "Backup failed for ${device}. Check permissions or device availability."
+        echo
     fi
 done
 
 echo "All LUKS headers have been backed up."
+
+echo
+
+# Set appropriate permissions on the backup folder (e.g., allow full control for the owner and read/execute permissions for group and others)
+sudo chmod -R 755 "$backup_dir"
+echo "Setting permissions on ${backup_dir} to 755 (allows full control for the owner and read/execute permissions for group and others)"
+
+echo
+
+# Change ownership of the backup file to the current user and inherit parent directory permissions
+sudo chown -R "$current_user:$current_user" "$backup_dir"
+echo "Changing ownership of ${backup_dir} to ${current_user} and inheriting permissions from parent directory."
